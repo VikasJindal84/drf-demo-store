8c774cf7d1ce78a8ac1ff8a8c37b959ddf12a29b
VikasJindal84 <vikasjindal1984@gmail.com>
2023-04-21 06:52:48 +0530

    Add Django Model for customer

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    fields = ["name", "age", "note", "email", "is_active"] # Fields to use for add/edit/show page
+    list_display = ["name", "email"] # fields to display in search page
+    list_display_links = ["name"] # fields that will be a link in search page
+
+admin.site.register(Customer, CustomerAdmin)
--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField('Customer', max_length=120)
+    age = models.IntegerField()
+    note = models.TextField(blank=True, null = True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self): 
+        return self.name
